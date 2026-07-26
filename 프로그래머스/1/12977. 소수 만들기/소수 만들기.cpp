4bdf195a0dd9ecefd67d4a8dcb974228cd57c3b2
jinny4069-creato <jinny4069@gmail.com>
2026-07-26 20:50:24 +0900

    [level 1] Title: 소수 만들기, Time: 44.48 ms, Memory: 4.82 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.cpp
@@ -0,0 +1,34 @@
+    #include <vector>
+    #include <iostream>
+    using namespace std;
+
+   int solution(vector<int> nums) {
+    int answer = 0;
+  
+    for (int i = 0; i < nums.size(); i++)
+    {
+        for (int j = i + 1; j < nums.size(); j++)
+        {
+            for (int k = j + 1; k < nums.size(); k++)
+            {
+                if (i == j || i == k || j == k)
+                    continue;
+                
+                int sum = nums[i] + nums[j] + nums[k];
+                int count = 0;
+                for (int q = 2; q < sum + 1; q++)
+                {
+                    if (q != sum && sum % q == 0)
+                        count++;
+                }
+                if (count == 0)
+                    answer++;
+            }
+        }
+    }
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    cout << "Hello Cpp" << endl;
+
+    return answer;
+}
