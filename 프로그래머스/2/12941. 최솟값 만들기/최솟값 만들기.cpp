e95a4cba3a77c516e54523e56bd0636e56a34d77
jinny4069-creato <jinny4069@gmail.com>
2026-08-09 18:51:25 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.09 ms, Memory: 4.5 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.cpp
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.cpp
@@ -8,11 +8,11 @@ int solution(vector<int> A, vector<int> B)
     int answer = 0;
  
     sort(A.begin(), A.end(), less<>());
-    sort(B.begin(), B.end(), less<>());
+    sort(B.begin(), B.end(), greater<>());
 
     for (int i = 0; i < A.size(); i++)
     {
-         answer += A[i] * B[B.size() - (i + 1)];
+         answer += A[i] * B[i];
     }
     return answer;
 }
